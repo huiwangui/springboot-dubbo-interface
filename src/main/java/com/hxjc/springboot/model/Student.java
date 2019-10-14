c7c912acfc62aa600f9d4401ee661f4b6b9715c9
wgh <1311639728@qq.com>
2019-10-14 14:58:17 +0800

    上传dubbo后台管理系统的jar包及zookeeper的jar包

--- src/main/java/com/hxjc/springboot/model/Student.java
+++ src/main/java/com/hxjc/springboot/model/Student.java
@@ -1,6 +1,8 @@
 package com.hxjc.springboot.model;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
     private Integer id;
 
     private String name;
